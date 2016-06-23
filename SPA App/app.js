74bcc459e070464412489257ef97778256398dd9
CyberSmoke <vivek.mannur@gmail.com>
2016-06-23 16:34:17 -0400

    SPA updates to add a new form

--- SPA App/app.js
+++ SPA App/app.js
@@ -30,12 +30,23 @@
 			if( setCurrent )
 			{
 				this.current = setCurrent;
-			}	
+			}
 			else
 			{
 				this.current = 0;
-			}	
+			}
 		};
+	});
+
+	app.controller("ReviewController", function()
+	{
+		this.newReview = {};
+		this.addReview = function( product )
+		{
+			product.reviews.push( this.newReview );
+			this.newReview = {};
+		};
+
 	});
 
 	var gems = [
@@ -44,17 +55,17 @@
 		description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.',
 		shine: 8,
 		price: 110.50,
-    	rarity: 7,
-    	color: '#CCC',
-    	faces: 14,
+	    	rarity: 7,
+    		color: '#CCC',
+	    	faces: 14,
 		canPurchase: true,
 		soldOut: false,
-      	images: [
-        	"images/gem-01.gif",
-        	"images/gem-02.gif",
-        	"images/gem-03.gif",
-        	"images/gem-04.gif"
-      	],
+      		images: [
+        		"images/gem-01.gif",
+	        	"images/gem-02.gif",
+        		"images/gem-03.gif",
+        		"images/gem-04.gif"
+	      	],
 		reviews: [
 		{
 			stars: 1,
@@ -77,69 +88,69 @@
 
 	},
 	{
-      	name: 'Bloodstone',
-      	description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
-      	shine: 9,
-      	price: 22.90,
-      	rarity: 6,
-      	color: '#EEE',
-      	faces: 12,
-      	canPurchase: true,
+      		name: 'Bloodstone',
+	      	description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+      		shine: 9,
+	      	price: 22.90,
+      		rarity: 6,
+	      	color: '#EEE',
+      		faces: 12,
+	      	canPurchase: true,
 		soldOut: false,
-      	images: [
-      	   	"images/gem-05.gif",
-        	"images/gem-06.gif",
-        	"images/gem-07.gif",
-        	"images/gem-08.gif"
-      	],
-      	reviews: [
-      	{
-        	stars: 3,
-        	body: "I think this gem was just OK, could honestly use more shine, IMO.",
-        	author: "JimmyDean@example.org",
-        	createdOn: 1397490980837
-      	},
-      	{
-        	stars: 4,
-        	body: "Any gem with 12 faces is for me!",
-        	author: "gemsRock@example.org",
-        	createdOn: 1397490980837
-      	}]
-    },
+      		images: [
+      	   		"images/gem-05.gif",
+	        	"images/gem-06.gif",
+        		"images/gem-07.gif",
+        		"images/gem-08.gif"
+	      	],
+      		reviews: [
+	      	{
+        		stars: 3,
+        		body: "I think this gem was just OK, could honestly use more shine, IMO.",
+	        	author: "JimmyDean@example.org",
+        		createdOn: 1397490980837
+	      	},
+      		{
+        		stars: 4,
+	        	body: "Any gem with 12 faces is for me!",
+        		author: "gemsRock@example.org",
+        		createdOn: 1397490980837
+	      	}]
+	},
 	{
-      	name: 'Zircon',
-      	description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-      	shine: 70,
-      	price: 1100,
-      	rarity: 2,
-      	color: '#000',
-      	faces: 6,
-      	canPurchase: true,
+      		name: 'Zircon',
+	      	description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+      		shine: 70,
+	      	price: 1100,
+      		rarity: 2,
+	      	color: '#000',
+      		faces: 6,
+	      	canPurchase: true,
 		soldOut: false,
-      	images: [
-        	"images/gem-09.gif",
-        	"images/gem-10.gif",
-        	"images/gem-11.gif",
-        	"images/gem-12.gif"
-      	],
-      	reviews: [
-      	{
-        	stars: 1,
-        	body: "This gem is WAY too expensive for its rarity value.",
-        	author: "turtleguyy@example.org",
-        	createdOn: 1397490980837
-      	},
-      	{
-        	stars: 1,
-        	body: "BBW: High Shine != High Quality.",
-        	author: "LouisW407@example.org",
-        	createdOn: 1397490980837
-      	},
-      	{
-        	stars: 1,
-        	body: "Don't waste your rubles!",
-        	author: "nat@example.org",
-        	createdOn: 1397490980837
-      	}]
-    }];
+      		images: [
+        		"images/gem-09.gif",
+	        	"images/gem-10.gif",
+        		"images/gem-11.gif",
+        		"images/gem-12.gif"
+	      	],
+      		reviews: [
+	      	{
+        		stars: 1,
+        		body: "This gem is WAY too expensive for its rarity value.",
+	        	author: "turtleguyy@example.org",
+        		createdOn: 1397490980837
+	      	},
+      		{
+        		stars: 1,
+	        	body: "BBW: High Shine != High Quality.",
+        		author: "LouisW407@example.org",
+        		createdOn: 1397490980837
+	      	},
+      		{
+        		stars: 1,
+	        	body: "Don't waste your rubles!",
+        		author: "nat@example.org",
+        		createdOn: 1397490980837
+	      	}]
+	}];
 })();
